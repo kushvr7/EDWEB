fc5ee2d026a2650ad61fda96c7cb65bf3d339a26
Kush <kushv2660@gmail.com>
2020-07-27 01:10:46 +0530

    footer-v1.0

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import Footer from './Footer/FooterM';
+
+function App() {
+  return (
+    <div className="App">
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+      <div>wrgegegetgte</div>
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer/FooterM.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './FooterC.css';
+function footerM() {
+    return (
+        <div>
+<div className="footer">
+  <div className="wrap">
+   <div className="d1">
+       <p>© 2020 Edbudz</p>
+   </div>
+   <div className="d2">
+       <h2>Quick Link</h2>
+       <pre>
+           
+
+<span id="d2p1"><a className="d2a" href="http://www.edbudz.in/home/">Home</a></span><br/><br/>
+<span id="d2p1"><a className="d2a" href="http://www.edbudz.in/about-us/">About Us</a></span><br/><br/>
+<span id="d2p1"><a className="d2a" href="http://www.edbudz.in/courses/">Courses</a></span><br/><br/>
+<span id="d2p1"><a className="d2a" href="http://www.edbudz.in/contact-us/">Contact Us</a></span>
+
+       </pre>
+   </div>
+   <div className="d3">
+       <h2>Contact Us</h2>
+       <pre>
+<span id="d3p1">Ashoka road,Shipra suncity</span><br/><br/>
+<span id="d3p1">Indirapuram,Ghaziabad</span><br/><br/>
+<span id="d3p1">Pin-201014</span><br/><br/>
+<span id="d3p1">+91 7983572504</span><br/><br/>
+<span id="d3p1">+91 8077953524</span><br/><br/>
+</pre>
+   </div>
+
+   <div className="d4">
+     <h2>Stay Connected</h2>
+     <div className="d4d">
+         <span>
+         <a id="d4a" className="icf" href="https://www.facebook.com/EdBudz.in/"><i class="fab fa-facebook fa-2x"></i></a>
+         <a id="d4a" className="icy" href="https://www.youtube.com/channel/UCIGwt6vmDmzWsx6V2H7LEhA"><i class="fab fa-youtube fa-2x"></i></a>
+         <a id="d4a" className="icl" href="https://www.linkedin.com/company/edbudz"><i class="fab fa-linkedin fa-2x"></i></a>
+         <a id="d4a" className="ici" href="https://www.instagram.com/edbudz_/"><i class="fab fa-instagram fa-2x"></i></a>
+         </span>
+     </div>
+   </div>
+
+  </div>
+</div>
+
+        </div>
+    )
+}
+
+export default footerM
